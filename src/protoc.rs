bfb99a1748f6289eef27401c686c73db2e2b5700
Spxg <itsme@unsafe.me>
2020-08-30 23:53:49 +0800

    create execute info && impl serde

--- src/protoc.rs
+++ src/protoc.rs
@@ -1,4 +1,14 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Operation {
-    Execute(String),
+    Execute(ExecuteInfo),
     Reload,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ExecuteInfo {
+    token: String,
+    command: String,
+}
+
