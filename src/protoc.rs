b2d15ffa00e7a07851921cd2e4aa26f07ded8367
Spxg <itsme@unsafe.me>
2020-09-01 10:50:03 +0800

    update protoc

--- src/protoc.rs
+++ src/protoc.rs
@@ -12,3 +12,26 @@ pub struct ExecuteInfo {
     command: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ExecuteResult {
+    Ok,
+    DeviceOffline,
+    CoreOffline,
+}
+
+impl ExecuteInfo {
+    pub fn new(token: &str, command: &str) -> Self {
+        Self {
+            token: token.into(),
+            command: command.into()
+        }
+    }
+
+    pub fn get_token(&self) -> String {
+        self.token.clone()
+    }
+
+    pub fn get_command(&self) -> String {
+        self.command.clone()
+    }
+}
